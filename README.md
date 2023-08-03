1300ceb99e8a8a5e85a5122e3053bdd0a6edd90c
Denis Casanuova <denis@casanuova.me>
2023-08-03 19:28:56 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,41 @@
+# Docker Template For Php 8.1
+
+## Requirement
+Make sure to have:
+- aws cli : `ls  ~/.aws`
+If not install it and configure: https://docs.aws.amazon.com/cli/latest/userguide/getting-started-install.html
+
+-  **mkcert** (https://github.com/FiloSottile/mkcert) e **docker/docker-compose**.
+
+## Configuration
+Exec command `bash configure.sh` and insert:
+
+- Local site host eg: `domain.localhost`
+- Network and project name eg: `my-project`
+- Github eg: `ghp_xxx`
+
+
+## Info
+The application will be available in dir `app`
+
+
+### To start the environment:
+```
+docker-compose  up -d
+```
+
+### To stop the environment:
+```
+docker-compose  down
+```
+
+### To exec command:
+```
+docker exec -w /var/www cm-docker_php_1 php bin/console 
+```
+
+### Navigtion
+
+```
+https://yourdomain.localhost/
+```
---
+++ configure.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+echo "Enter host (eg: domain.localhost)"
+# shellcheck disable=SC2162
+read host
+
+echo "Enter network (eg: my-project)"
+# shellcheck disable=SC2162
+read network
+
+echo "GithubToken (eg: ghp_xxx)"
+# shellcheck disable=SC2162
+read gitHubToken
+
+# Replace in file function
+replaceInFiles () {
+  sed -i'.bak'  -e"s/\${network}/$network/; s/\${host}/$host/; s/\${gitHubToken}/$gitHubToken/" "$1"
+  rm "$1"".bak"
+}
+
+replaceInFiles "./nginx/conf.d/conf.conf"
+replaceInFiles "./traefik/config.yml"
+replaceInFiles "./.env"
+replaceInFiles "./docker-compose.yml"
+
+
+mkdir certs && cd certs && mkcert -install local-cert && cd ..
+mkcert -cert-file certs/local-cert.pem -key-file certs/local-key.pem "$network" "*.$network"
+
+docker network create "$network"
+
+echo "App available in: $host";
+echo "The application will be available in dir: app";
+echo "Start environment with docker-compose  up -d";
+echo "God work 😀";
+
